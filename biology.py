b39ed9025bf9226f1e3688e4d1d04012996f505b
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-04-25 15:09:34 -0400

    Updated from Brython Server: 4/25/2019 3:10:09 PM

--- biology.py
+++ biology.py
@@ -11,21 +11,18 @@ data = {
     '5': 'You have Trisomy 5p. This disorder is characterized by growth delays after birth, malformations of the head and facial (craniofacial) area, and abnormalities of the hands and feet.'
     '6': 'You have Partial Trisomy 6q. This disorder is characterized by growth delays before and after birth, severe to profound mental retardation, a delay in the acquisition of skills requiring coordination of muscular and mental activity (psychomotor retardation), distinctive malformations of the skull and facial (craniofacial) region, musculoskeletal abnormalities, and/or additional physical features.'
     '7': 'You have Partial Monosomy 7p. This disorder is characterized by growth deficiency, musculoskeletal abnormalities, genital defects, structural malformations of the heart that are present at birth (congenital heart defects), and/or other abnormalities.'
-    '8': 'You have Mosaic trisomy 8. This disorder is characterized by brain malformations, highly arched or cleft palate, shortened neck with extra skin folds, long slim body with a narrow chest, shoulders, and pelvis and kidney and cardiac abnormalities. 
-
-
-
-    '9':  '
-    '10': '
-    '11': '
-    '12': '
-    '13': '
-    '14': ' 
-    '15': '  
-    '16': '
-    '17': '
-    '18': '
-    '19': '
+    '8': 'You have Mosaic trisomy 8. This disorder is characterized by brain malformations, highly arched or cleft palate, shortened neck with extra skin folds, long slim body with a narrow chest, shoulders, and pelvis and kidney and cardiac abnormalities.' 
+    '9':  'You have Mosaic Trisomy 9. This disorder is characterized by growth deficiency beginning before birth, failure to grow and gain weight at the expected rate (failure to thrive) during infancy, and low muscle tone (hypotonia).'
+    '10': 'You have Distal Trisomy 10q. This disorder is characterized by abnormally slow growth before and after birth, malformations of the head and facial (craniofacial) area, a small nose with turned up nostrils (anteverted nares) and a broad, flat, and/or depressed nasal bridge, a small, bow-shaped mouth with a prominent upper lip,  an unusually small lower jaw, and/or, in some patients, incomplete closure of the roof of the mouth (cleft palate).'
+    '11': 'You have Partial Trisomy 11q. This disorder is characterized by growth retardation before and after birth, delayed acquisition of skills requiring the coordination of mental and motor activities (psychomotor retardation), mild to moderate mental retardation, and distinctive craniofacial abnormalities.'
+    '12': 'You have Trisomy 12p. This disorder is characterized by macrocephaly (unusually large head), abnormal muscle tone, characteristic facial features, developmental delay and intellectual disability.'
+    '13': 'You have Patau syndrome or Trisomy 13. This disorder is characterized by severe intellectual disability and many physical abnormalities, such as congenital heart defects, brain or spinal cord abnormalities, very small or poorly developed eyes (microphthalmia), extra fingers or toes, cleft lip with or without cleft palate, and weak muscle tone (hypotonia).'
+    '14': 'You have Mosaic Trisomy 14. This disorder is characterized by rowth delays before birth (intrauterine growth retardation), failure to grow and gain weight at the expected rate (failure to thrive) during infancy, delays in the acquisition of skills requiring the coordination of mental and physical abilities (psychomotor delays), and mental retardation.'
+    '15': 'You have Trisomy 15q. This disorder is characterized by developmental delay, intellectual disability, hypotonia (low muscle tone), seizures; high and/or cleft palate (roof of the mouth), scoliosis; slow growth, communication difficulties, behavioral problems, and distinctive facial features.' 
+    '16': 'You have Trisomy 16. This disorder is characterized by cardiac malformations, hypospadias, two vessel cords, clinodactyly and pulmonary hypoplasia.'
+    '17': 'You have Trisomy 17. This disorder is characterized by developmental delays, body asymmetry, slow growth, and cerebellar hypoplasia.'
+    '18': 'You have Edwards' ''' ' syndrome or Trisomy 18. This disorder is characterized by slow growth before birth (intrauterine growth retardation), a low birth weight, heart defects and abnormalities of other organs that develop before birth, a small, abnormally shaped head, a small jaw and mouth, and clenched fists with overlapping fingers.
+    '19': 'You have 
     '20': ' 
     '21': '
     '22': '
